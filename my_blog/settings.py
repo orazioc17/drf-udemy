03e314dc7ec5e073e9458f03fe05214f16f82fa9
Orazio Cappadonna <oraziocappadonna1999@gmail.com>
2022-03-15 20:13:43 -0400

    Agregada documentacion

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
 
     'posts',
 ]
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
